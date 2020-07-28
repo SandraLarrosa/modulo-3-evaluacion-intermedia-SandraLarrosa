c148a82cbdd70c2249c63b079f3e8b4728f51c23
Alexandra <sandra.larrosa88@gmail.com>
2020-07-28 08:45:50 +0200

    Add list Pokemon in json

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import listPokemon from '../data/listPokemon.json'
 import '../stylesheets/App.scss';
 
 function App() {
+  console.log(listPokemon);
+
   return (
     <div className="App">
       <h1>Hola Mundo</h1>
